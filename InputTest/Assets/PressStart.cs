942cff288402133041da402747cfb3023a3f99e3
Jullyana <jullyanavycas@gmail.com>
2016-03-12 18:26:10 -0800

    Added data structure and player creation function
    InputTest is mostly done - I'll work on input directly in our project
    from now on.

--- InputTest/Assets/PressStart.cs
+++ InputTest/Assets/PressStart.cs
@@ -7,10 +7,13 @@ namespace UnityStandardAssets._2D
 
         public GameObject player;
 
+        private bool []free;
+
         // Use this for initialization
         void Start()
         {
-
+            free = new bool[2];
+            free[0] = free[1] = true;
         }
 
         // Update is called once per frame
@@ -20,20 +23,26 @@ namespace UnityStandardAssets._2D
             if (Input.GetButtonUp("Start1"))
             {
                 Vector3 position = new Vector3(5, 0, 0);
-                GameObject player1 = Instantiate(player, position, Quaternion.identity) as GameObject;
-                // set player number to the correct control
-                PlatformerCharacter2D script1 = player1.GetComponent<PlatformerCharacter2D>();
-                script1.SetPlayerNumber(1);
+                CreatePlayer(1, position);
             }
 
             if (Input.GetButtonUp("Start2"))
             {
                 Vector3 position = new Vector3(-3, 5, 0);
-                GameObject player2 = Instantiate(player, position, Quaternion.identity) as GameObject;
-                // set player number to the correct control
-                PlatformerCharacter2D script2 = player2.GetComponent<PlatformerCharacter2D>();
-                script2.SetPlayerNumber(2);
+                CreatePlayer(2, position);
             }
         }
+
+        void CreatePlayer(int player_num, Vector3 pos)
+        {
+            if (free[player_num-1])
+            {
+                free[player_num-1] = false;
+                GameObject p = Instantiate(player, pos, Quaternion.identity) as GameObject;
+                PlatformerCharacter2D script = p.GetComponent<PlatformerCharacter2D>();
+                script.SetPlayerNumber(player_num);
+            }
+        }
+
     }
 }
