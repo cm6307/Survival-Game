7a8eb0c563c0025316988490299990e15b36b4aa
aquariumm <aquariumm@hotmail.com>
2016-03-26 19:38:16 -0400

    UI Design
    UI Design for the game, StartScreen, LoginScreen, RegisterScreen,
    MenuScreen. For LoginScreen, the username, password can be captured by
    LoginMenuInput script.The background is now using built-in background,
    the previous Skybox feature not exist because the 2d assets folder was
    removed to save space.

---
+++ UserInterface/Assets/ScriptScenes/LoginMenuInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginMenuInput : MonoBehaviour {
+
+    [SerializeField]
+    private InputField nameInputField = null;
+
+    [SerializeField]
+    private Button submitButton = null;
+
+    private void Start()
+    {
+        // Add listener to catch the submit
+        var input = gameObject.GetComponent<InputField>();
+        var se = new InputField.SubmitEvent();
+        se.AddListener(SubmitName);
+        input.onEndEdit = se;
+
+        // Add validation
+       // nameInputField.characterValidation = InputField.CharacterValidation.Alphanumeric;
+
+    }
+
+    private void SubmitName(string name)
+    {
+        //What to do with the value from input field
+        Debug.Log(name);
+    }
+}
