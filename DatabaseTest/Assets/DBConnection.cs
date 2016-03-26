4da2fe22789020e02f558716bc1b8963c305e7ac
Jullyana <jullyanavycas@gmail.com>
2016-03-25 20:03:14 -0700

    DatabaseTest added
    Building the database subsystem in this project, which will be merged
    into the main one later on.

---
+++ DatabaseTest/Assets/DBConnection.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class DBConnection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string cs = @"server=localhost;userid=root;
+            password=25698914;database=survival_game";
+
+        MySqlConnection conn = new MySqlConnection(cs);
+
+        try
+        {
+            conn.Open();
+            Debug.Log("MySQL version : " + conn.ServerVersion);
+        }
+        catch (MySqlException ex)
+        {
+            Debug.Log("Error: " + ex.ToString());
+
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+    
+}
---
+++ DatabaseTest/Assets/DatabaseConnection.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+using System.Data;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class DatabaseConnection : MonoBehaviour {
+
+    public string host, database, user, password;
+    public bool pooling = true;
+
+    private string connectionString;
+    private MySqlConnection connection = null;
+    private MySqlCommand command = null;
+    private MySqlDataReader reader = null;
+    private MD5 _md5Hash;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        connectionString = "Server=" + host + ";Database=" + database + ";User=" + user + ";Password=" + password + ";Pooling=";
+        if (pooling)
+        {
+            connectionString += "true;";
+        }
+        else {
+            connectionString += "false;";
+        }
+
+        try
+        {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            Debug.Log("Connection to database was successful.");
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (connection != null)
+        {
+            if (connection.State.ToString() != "Closed")
+            {
+                connection.Close();
+                Debug.Log("MySql connection closed. Goodbye!");
+            }
+            connection.Dispose();
+        }
+    }
+
+}
