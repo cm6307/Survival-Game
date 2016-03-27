455d89f1f106541c8451892cf42a9aac46b53ce9
Jullyana <jullyanavycas@gmail.com>
2016-03-26 19:54:40 -0700

    Login function added
    ProfileManager now can login users.

---
+++ DatabaseTest/Assets/ProfileManagement/SessionManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SessionManager : MonoBehaviour {
+
+    public Facade f;
+    private string[] LoggedUsers = new string[4];
+    private int[] PointsEarned = new int[4];
+
+    void Start()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            LoggedUsers[i] = null;
+        }
+        Login("dummie", "1234");
+        Debug.Log("LoggedIn users: ");
+        for (int i = 0; i < 4; i++)
+        {
+            if (LoggedUsers[i] != null)
+            {
+                Debug.Log(LoggedUsers[i]);
+            }
+        }
+    }
+
+    public void Login(string username, string password)
+    {
+        if(f.CheckPassword(username, password))
+        {
+            bool loggedIn = false;
+            for(int i = 0; i < 4; i++)
+            {
+                if(LoggedUsers[i] == null)
+                {
+                    loggedIn = true;
+                    LoggedUsers[i] = username;
+                    PointsEarned[i] = 0;
+                    break;
+                }
+            }
+            if (!loggedIn)
+            {
+                throw new System.InvalidOperationException("No more than 4 users can be logged in at a time.");
+            }
+        }
+        else
+        {
+            throw new System.InvalidOperationException("Wrong password.");
+        }
+    }
+
+}
